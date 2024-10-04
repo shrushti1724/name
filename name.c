4f04ac7f9e8301f55a9ec1a15811e18c3898da82
shrushti1724 <shrustipatel0817@gmail.com>
2024-10-04 15:47:10 +0530

    Add files via upload

---
+++ name.c
@@ -0,0 +1,230 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,j;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1 || i==5 || i==3 )
+			{
+				printf("* ");
+			}
+			else if(i==2 && j==1)
+			{
+				printf("* ");
+			}
+			else if(i==4 && j==5)
+			{
+				printf("* ");
+			}
+			else if(i==2 && j==1)
+			{
+				printf("* ");
+			}
+			else if(i==4 && j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+			
+		}
+		printf("\n");
+	}
+	   
+	   
+	   printf("\n");
+	
+	
+	
+	
+	   
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if (j==1||i==3||j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	 
+	 
+	 
+		printf("\n");
+		
+		
+	
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=5;j++)
+			{
+				if(i==1 || i==3 || j==1 || i==2 && j==5)
+				{
+					printf("* ");
+				}
+				else if(i==4 && j==3)
+				{
+					printf("* ");
+					
+				}
+				else if(i==5 && j==4)
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+					
+				}
+			}
+			printf("\n");
+		}
+		
+		
+		printf("\n");
+		
+		
+
+		
+		
+			for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=5;j++)
+			{
+				
+				if( i==5 || j==1||j==5)
+				{
+					printf("* ");
+				}
+				else
+				{
+					printf("  ");
+				}
+				
+			}
+			printf("\n");
+		}
+		
+		
+		printf("\n");
+		
+		
+		
+		for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1 || i==5 || i==3 )
+			{
+				printf("* ");
+			}
+			else if(i==2 && j==1)
+			{
+				printf("* ");
+			}
+			else if(i==4 && j==5)
+			{
+				printf("* ");
+			}
+			else if(i==2 && j==1)
+			{
+				printf("* ");
+			}
+			else if(i==4 && j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+			
+		}
+		printf("\n");
+	}
+	   
+	   
+	   printf("\n");
+	
+	
+	
+	
+	   
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if (j==1||i==3||j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	 
+	 
+	 
+		printf("\n");
+		
+		
+	
+		for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if (i==1||j==3)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	 
+	printf("\n");	
+		
+		for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if (i==1||j==3 ||i==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	 
+		
+		
+		
+		
+		 
+		 
+
+}
+
